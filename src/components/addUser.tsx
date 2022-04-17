6765512e6ad5e998771c19def8400b3d742a2c56
Amin Arshadinia <a.arshadinia@novin.dev>
2022-04-17 14:31:53 +0430

    feat: change user add form styles

--- src/components/addUser.tsx
+++ src/components/addUser.tsx
@@ -2,15 +2,23 @@ import React, { Fragment, useState } from "react";
 import styled from "@emotion/styled";
 import { useNavigate } from "react-router-dom";
 import { useAddUser } from "../hooks/useAddUser";
-import { Button, CircularProgress, Container, TextField } from "@mui/material";
+import { Button, Container, LinearProgress, TextField } from "@mui/material";
 import { ErrorSwal, successSwal } from "../utilities/swal/swal";
 import Navbar from "./navbar";
+import DoneIcon from "@mui/icons-material/Done";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
+const Division = styled.section`
+  border: 1px solid #c4c4c4c4;
+  border-radius: 10px;
+  padding: 5%;
+  margin: 10%;
+  box-shadow: 5px 10px 8px #888888;
+`;
 const AuthForm = styled.section`
   display: flex;
   align-item: center;
-  width: 50%;
-  margin: 0 auto;
+  margin: auto;
 `;
 
 const AddUserPage = () => {
@@ -40,24 +48,49 @@ const AddUserPage = () => {
     <Fragment>
       <Navbar />
       <Container>
-        <AuthForm>
-          {isLoading && <CircularProgress size={24} />}
-          <TextField
-            value={name}
-            onChange={(e) => setUserData({ name: e.target.value, job })}
-            id="standard-basic"
-            label="name"
-            variant="standard"
-          />
-          <TextField
-            value={job}
-            onChange={(e) => setUserData({ name, job: e.target.value })}
-            id="standard-basic"
-            label="job"
-            variant="standard"
-          />
-          <Button onClick={addUser}>Submit</Button>
-        </AuthForm>
+        <Division>
+          <p>Add new user</p>
+          <AuthForm>
+            <TextField
+              fullWidth
+              style={{ marginRight: "30px" }}
+              value={name}
+              onChange={(e) => setUserData({ name: e.target.value, job })}
+              id="standard-basic"
+              label="name"
+              variant="outlined"
+            />
+            <TextField
+              fullWidth
+              value={job}
+              onChange={(e) => setUserData({ name, job: e.target.value })}
+              id="standard-basic"
+              label="job"
+              variant="outlined"
+            />
+          </AuthForm>
+          <section style={{ margin: "30px 0" }}>
+            <Button
+              onClick={addUser}
+              style={{ marginRight: "20px" }}
+              variant="contained"
+              color="success"
+              startIcon={<DoneIcon />}
+            >
+              Submit
+            </Button>
+            <Button
+              onClick={() => navigate(-1)}
+              variant="contained"
+              color="warning"
+              startIcon={<ArrowBackIcon />}
+            >
+              Return
+            </Button>
+          </section>
+
+          {isLoading && <LinearProgress />}
+        </Division>
       </Container>
     </Fragment>
   );
