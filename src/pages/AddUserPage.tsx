5a86b8d721423f4c2bc075b2e92f0aa763a5f2b3
Amin Arshadinia <a.arshadinia@novin.dev>
2022-04-18 11:16:04 +0430

    feat: create add user page

---
+++ src/pages/AddUserPage.tsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import AddUser from "../components/AddUser";
+
+const AddUserPage = () => {
+  return (
+    <Fragment>
+      <AddUser />
+    </Fragment>
+  );
+};
+
+export default AddUserPage;
