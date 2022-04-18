57e271b58bf46fb202bbc53a7218ca004792b1d6
Amin Arshadinia <a.arshadinia@novin.dev>
2022-04-18 11:20:53 +0430

    feat: error swal accept message

--- src/utilities/swal/swal.ts
+++ src/utilities/swal/swal.ts
@@ -1,23 +1,20 @@
 import { NavigateFunction } from "react-router-dom";
 import Swal from "sweetalert2";
 
-export const successSwal = (route: string , navigate: NavigateFunction) => {
-
+export const successSwal = (route: string, navigate: NavigateFunction) => {
   Swal.fire({
     icon: "success",
     title: "Operation Succeeded...",
-    // text: "Something went wrong!",
   }).then((res) => {
-    if(res.isConfirmed){
-        navigate(route);
+    if (res.isConfirmed) {
+      navigate(route);
     }
-    
   });
-}
+};
 
-export function ErrorSwal() {
+export function ErrorSwal(message: string) {
   Swal.fire({
     icon: "error",
-    title: "Something went wrong!",
+    title: message ? message : "Something went wrong!",
   });
 }
