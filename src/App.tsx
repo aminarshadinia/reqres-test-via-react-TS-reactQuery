5cc01a85762fcc694f36d2e5ce71ab4870cff567
Amin Arshadinia <a.arshadinia@novin.dev>
2022-04-18 17:08:40 +0430

    feat: create 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./App.css";
+import NotFoundPage from "./pages/404/NotFoundPage";
 import AddUserPage from "./pages/AddUserPage";
 import LoginPage from "./pages/LoginPage";
 import UpdateUserPage from "./pages/UpdateUserPage";
@@ -13,6 +14,7 @@ const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="*" element={<NotFoundPage />} />
         <Route path="/" element={<LoginPage />} />
         <Route element={<ProtectedRoutes />}>
           <Route path="/user-list" element={<UserListPage />} />
