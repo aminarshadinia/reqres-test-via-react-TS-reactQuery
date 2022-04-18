0894f525f4599e98c53fdb6a1476d5590de4a20d
Amin Arshadinia <a.arshadinia@novin.dev>
2022-04-18 11:16:59 +0430

    feat: create user details page

---
+++ src/pages/UserDetailsPage.tsx
@@ -0,0 +1,12 @@
+import { Fragment } from 'react'
+import UserDetails from '../components/UserDetails'
+
+const UserDetailsPage = () => {
+  return (
+    <Fragment>
+      <UserDetails />
+    </Fragment>
+  )
+}
+
+export default UserDetailsPage
