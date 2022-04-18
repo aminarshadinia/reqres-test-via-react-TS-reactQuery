0c13856b8929edd9baec1890164ef7dec555f5e4
Amin Arshadinia <a.arshadinia@novin.dev>
2022-04-18 11:20:15 +0430

    feat: add on error in submit function

--- src/hooks/useSubmit.jsx
+++ src/hooks/useSubmit.jsx
@@ -1,12 +1,14 @@
 import { useMutation } from "react-query"
 import { onLogin } from "../components/api/api"
 
-export const useSubmit = ({onSuccess}) => {
+export const useSubmit = ({onSuccess,onError}) => {
   const {mutate , isLoading , data} = useMutation("handleLogin" ,onLogin , {
     onSuccess : (variables) => {
-     
       onSuccess(variables)
     },
+    onError : (variables) => {
+      onError(variables)
+    }
   })
   return {mutate , isLoading , data}
 }
