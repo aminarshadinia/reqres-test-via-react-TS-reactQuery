20d5dd574d80c04187545cac9756f723c6b64d25
Amin Arshadinia <a.arshadinia@novin.dev>
2022-04-18 14:30:16 +0430

    feat: add protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import './App.css';
-import AddUserPage from './pages/AddUserPage';
-import LoginPage from './pages/LoginPage';
-import UpdateUserPage from './pages/UpdateUserPage';
-import UserDetailsPage from './pages/UserDetailsPage';
-import UserListPage from './pages/UserListPage';
+import "./App.css";
+import AddUserPage from "./pages/AddUserPage";
+import LoginPage from "./pages/LoginPage";
+import UpdateUserPage from "./pages/UpdateUserPage";
+import UserDetailsPage from "./pages/UserDetailsPage";
+import UserListPage from "./pages/UserListPage";
+import ProtectedRoutes from "./ProtectedRoutes";
 
-const App:React.FC = () => {
+const App: React.FC = () => {
   return (
-      <BrowserRouter>
+    <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/user-list" element={<UserListPage />} />
-        <Route path="/add-user" element={<AddUserPage />} />
-        <Route path="/user-details/:id" element={<UserDetailsPage />} />
-        <Route path="/update-user/:id" element={<UpdateUserPage />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/user-list" element={<UserListPage />} />
+          <Route path="/add-user" element={<AddUserPage />} />
+          <Route path="/user-details/:id" element={<UserDetailsPage />} />
+          <Route path="/update-user/:id" element={<UpdateUserPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
