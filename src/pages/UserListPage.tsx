055f963a8a63295252a92875c6e22b7c76457fd4
Amin Arshadinia <a.arshadinia@novin.dev>
2022-04-18 11:17:11 +0430

    feat: create user list page

---
+++ src/pages/UserListPage.tsx
@@ -0,0 +1,12 @@
+import { Fragment } from 'react'
+import UserList from '../components/UserList'
+
+const UserListPage = () => {
+  return (
+    <Fragment>
+      <UserList />
+    </Fragment>
+  )
+}
+
+export default UserListPage
