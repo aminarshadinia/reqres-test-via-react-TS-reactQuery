670c34a22c52c7726525e5a74a42a6e803fe1371
Amin Arshadinia <a.arshadinia@novin.dev>
2022-04-20 14:36:09 +0430

    feat: change imported type name

--- src/hooks/useSubmit.tsx
+++ src/hooks/useSubmit.tsx
@@ -1,20 +1,8 @@
 import { useMutation } from "react-query";
 import { onLogin } from "../components/api/api";
-import { LoginArgs, LoginData } from "../components/interfaces/interfaces";
-// type Variables = { token: string };
-// type Arrgs = ({
-//   onSuccess,
-//   onError,
-// }: {
-//   onSuccess: (variables: Variables) => void;
-//   onError: (variables: string) => void;
-// }) => {
-//   mutate: UseMutateFunction<any, unknown, Credentials, unknown>;
-//   isLoading: boolean;
-//   data: any;
-// };
+import { HookType, LoginData } from "../components/interfaces/interfaces";
 
-export const useSubmit: LoginArgs = ({ onSuccess, onError }) => {
+export const useSubmit: HookType = ({ onSuccess, onError }) => {
   const { mutate, isLoading } = useMutation("handleLogin", onLogin, {
     onSuccess: (data: LoginData) => {
       onSuccess(data);
